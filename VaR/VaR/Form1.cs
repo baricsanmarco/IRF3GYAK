57e2547a9377033f74243a42ede26b60c5f31494
baricsanmarco <mark.baricsan@stud.uni-corvinus.hu>
2021-10-22 10:57:49 +0200

    Add GetPortfolioValue

--- VaR/VaR/Form1.cs
+++ VaR/VaR/Form1.cs
@@ -22,6 +22,53 @@ namespace VaR
             Ticks = context.Ticks.ToList(); //nem csak másik formátum, hanem másolatot készít az adatokból a lokál memóriában és ott dolgozunk vele, nem az SQL DB-n
             dataGridView1.DataSource = Ticks;
             CreatePortfolio();
+
+            /*int elemszám = Portfolio.Count();
+            decimal részvényekSzáma = (from x in Portfolio
+                                       select x.Volume).Sum();
+            var otp = from x in Ticks
+                      where x.Index.Trim().Equals("OTP")
+                      select new
+                      { 
+                          x.Index,
+                          x.Price
+                      }
+                      ;
+            Console.WriteLine("OTP darabszám: " + otp.Count());
+            //mivel a fenti var is egy lista lehet továbbszűrni
+
+            var top = from o in otp
+                      where o.Price > 7000
+                      select o;
+            Console.WriteLine("OTP darabszám nagyobb mint 7000: " + otp.Count().ToString());
+            //MessageBox.Show(string.Format)
+
+            var topsum = (from t in top
+                          select t.Price).Sum();
+
+            //c. A legrégebbi kereskedési nap:
+            DateTime minDátum = (from x in Ticks select x.TradingDay).Min();
+
+            //d. A legutolsó kereskedési nap:
+            DateTime maxDátum = (from x in Ticks select x.TradingDay).Max();
+            int elteltNapokSzáma = (maxDátum - minDátum).Days;
+
+
+            //g. Össze is lehet kapcsolni dolgokat, ez már bonyolultabb:
+            var kapcsolt =
+                from
+                    x in Ticks
+                join
+                    y in Portfolio
+            on x.Index equals y.Index
+                select new
+                {
+                    Index = x.Index,
+                    Date = x.TradingDay,
+                    Value = x.Price,
+                    Volume = y.Volume
+                };
+            dataGridView1.DataSource = kapcsolt.ToList();*/
         }
 
         private void CreatePortfolio()
@@ -36,6 +83,20 @@ namespace VaR
             Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
 
             dataGridView2.DataSource = Portfolio;
+        }
+        private decimal GetPortfolioValue(DateTime date)
+        {
+            decimal value = 0;
+            foreach (var item in Portfolio)
+            {
+                var last = (from x in Ticks
+                            where item.Index == x.Index.Trim() //trim azért kell, mert nchar(15)-ként van letárolva, ezért a üres részek ki vannak töltve 
+                               && date <= x.TradingDay
+                            select x)
+                            .First();
+                value += (decimal)last.Price * item.Volume;
+            }
+            return value;
         }
     }
 }
