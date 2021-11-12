23af17a40c34ae5091df6fb291ee2390059031c8
baricsanmarco <mark.baricsan@stud.uni-corvinus.hu>
2021-11-12 10:54:34 +0100

    Autó hozzáadása

---
+++ Factory pattern/Factory pattern/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using Factory_pattern.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_pattern.Entities
+{
+    public class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            var image = Image.FromFile(@"Images\car.png");
+            g.DrawImage(image, 0, 0, Width, Height); //kép, elhelyezkedés, átméretezés megfelelő méretre = toy ősosztályból vettük át a beállításokat
+        }
+    }
+}
